4c37b14c21791ed04135cac838ff98d90031a3f2
Sude Önder <sudeonder999@gmail.com>
2024-05-15 00:21:59 +0300

    newItem screen

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/data/dummy_items.dart';
+import 'package:shopping_app/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Grocery List'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _addItem,
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
         itemBuilder: (context, index) => ListTile(
           title: Text(groceryItems[index].name),
-          leading: CircleAvatar(
-            backgroundColor: groceryItems[index].category.color,
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
           ),
         ),
       ),
